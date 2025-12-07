89f3f89a4036b8406b51fda9619b059660c17587
atkac <tkachanastasiia10@gmail.com>
2025-12-07 19:47:33 +0200

    I add some comments and might change somthing in game to make my map work

--- backend/src/gamestate.py
+++ backend/src/gamestate.py
@@ -11,13 +11,17 @@ class Player:
         self.scores.append(abs(score))
 
 class Game:
+    """
+    global state of the game, active players, rounds, and the current status
+
+    """
     def __init__(self):
-        self.ndist = 6
+        self.ndist = 10     # Number of distances/rounds per game
         self.dist = []
         self.sumdist = 0
-        self.fin = True
+        self.fin = True     # True = Game over/Not started. False = Game running.
         self.players = {}
-        self.lb = []
+        self.lb = []    # Leaderboard
 
     def newGame(self):
         self.fin = False
@@ -27,9 +31,17 @@ class Game:
         return id in self.players.keys()
 
     def createPlayer(self, n) -> int:
+        """
+        Registers a new player into the game.
+        Args:
+            n: Name of the player
+        Returns:
+            id: The generated 5-digit ID for the player
+        """
         id = genId()
         # SQL STUFF HERE
         self.players[id] = Player(n, id)
+        self.fin = False
         return id
 
     def handleGuess(self, g) -> tuple:
